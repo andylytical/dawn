5fb84faa9c2e82a644f866f09825088fa82c1ea7
Andy Loftus <aloftus@gmail.com>
2023-12-30 04:58:09 +0000

    schedule links and exit

--- mk_links.js
+++ mk_links.js
@@ -1,30 +1,47 @@
-async function delay_open_links () {
+function delay_open_links () {
   var delay_min = 10000;
   var delay_max = 21000;
+  var num_links = 30;
+  var links_per_phase = 4;
+  var phase_pause = 10 * 60000; //10 minutes in ms
   var url_base = "https://www.bing.com/search?qs=n&form=QBRE&sp=-1&q="
-  var html = "";
   var search_word;
-  var size;
   var link;
-  for (i=0; i<=4; i++) {
-    size = rand_int( 8, 11 );
-    search_word = GPW.pronounceable( size );
-    console.log( "Size:" + size + " Word:" + search_word );
+  var prev_delay = 0;
+  var delay;
+  for (i=0; i < num_links; i++) {
+    // make url
+    search_word = GPW.pronounceable( rand_int(8, 11) );
     link = url_base + search_word;
     console.log(i + " " + link);
-    window.open( link, "_blank" );
-    await sleep_random( delay_min, delay_max );
-    console.log( "wake up" );
+    // calculate delay
+    delay = prev_delay + rand_int( delay_min, delay_max );
+    if ( i > 0 ) {
+      if ( i % links_per_phase === 0 ) {
+        // start of a new phase
+        delay += phase_pause;
+      }
+    }
+    else {
+      delay = 0;
+    }
+    // schedule open new link
+    setTimeout( open_link_in_new_tab, delay, link );
+    console.log( "  scheduled in " + delay + " milliseconds" );
+    // save last delay
+    prev_delay = delay;
   }
   console.log( "END" );
 }
 
-function rand_int(min, max) {
-  return Math.floor(Math.random() * (max - min + 1)) + min;
+
+function open_link_in_new_tab( url ) {
+  var now = new Date();
+  console.log( now.toLocaleTimeString() + ": window.open(" + url + ")" );
+  window.open( url, "_blank" );
 }
 
-async function sleep_random(min, max) {
-  var pause = rand_int(min, max);
-  console.log("pause " + pause);
-  return new Promise( resolve => setTimeout( resolve, pause ) );
+
+function rand_int(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 }
